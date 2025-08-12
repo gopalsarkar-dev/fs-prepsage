b08b772a3f7a36234d716fc26280b9e9f2fbae75
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-12 10:49:55 +0530

    create user profile menu and update topbar

---
+++ src/components/profilemenu/UserMenu.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { User } from "lucide-react";
+
+const UserMenu = () => {
+	return (
+		<>
+			<DropdownMenu>
+				<DropdownMenuTrigger
+					asChild
+					className="cursor-pointer">
+					<Avatar>
+						<AvatarFallback>R</AvatarFallback>
+					</Avatar>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent
+					className="mt-6 w-56"
+					align="end">
+					<DropdownMenuLabel>My Account</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuGroup>
+						<DropdownMenuItem>
+							<Link
+								href="/profile"
+								className="flex items-center gap-2 font-semibold">
+								<User />
+								<span>Profile</span>
+							</Link>
+						</DropdownMenuItem>
+
+						<DropdownMenuItem>
+							<div className="">Dark Mode</div>
+						</DropdownMenuItem>
+					</DropdownMenuGroup>
+					<DropdownMenuSeparator />
+
+					<DropdownMenuItem>
+						<div className="">Logout</div>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</>
+	);
+};
+
+export default UserMenu;
