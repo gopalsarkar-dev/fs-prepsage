5d67a4c5effe1602f7258704727bd6bef9178d82
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-14 09:30:56 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,18 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import prisma from "./db/prisma";
+import { serverEnv } from "./env/serverEnv";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+	secret: serverEnv.BETTER_AUTH_SECRET,
 	database: prismaAdapter(prisma, {
 		provider: "sqlite",
 	}),
+	emailAndPassword: {
+		enabled: true,
+		autoSignIn: false,
+	},
+
+	plugins: [nextCookies()],
 });
