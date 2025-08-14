11ba5086ff1b64325acfe48356be3c22e16d8c52
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-14 09:17:22 +0530

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,9 @@
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/client";
+import { clientEnv } from "./env/clientEnv";
+import { auth } from "./auth";
 export const authClient = createAuthClient({
 	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: "http://localhost:3000",
+	baseURL: clientEnv.NEXT_PUBLIC_BETTER_AUTH_URL,
+	plugins: [inferAdditionalFields<typeof auth>()],
 });
