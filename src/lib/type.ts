11765d07a52f4606e0175944ce1e02e0ddc6bfd7
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-23 19:04:55 +0530

    Update type.ts

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -6,3 +6,11 @@ export type LoginFormType = z.infer<typeof loginFormSchema>;
 export type SignupFormType = z.infer<typeof signupFormSchema>;
 
 export type UserPostType = z.infer<typeof userPostSchema>;
+
+export type PostType = {
+	id: string;
+	qtype: string;
+	question: string;
+	answer: string;
+	userId: string;
+};
